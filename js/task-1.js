6ac61682bfab3c19ee6db95c6c25108b78cd62a0
Nazar Bekerskij <bekerskijnazar9@gmail.com>
2025-11-16 13:23:15 +0200

    123

--- js/task-1.js
+++ js/task-1.js
@@ -1,7 +1,26 @@
 // 1.Напиши скрипт в якому є три радіобатони зі значенями кольрів. При обиранніна будь якого колір фону боді змінюється відповідно
 
-const inputRef = document.querySelectorAll(".input")
+const inputRef = document.querySelectorAll('input[name="color"]')
 
-inputRef.addEventListener("click", (event) => {
-        
+inputRef.forEach((input) => {
+  input.addEventListener("change", (event) => {
+   console.log(event.target.value);
+   document.body.style.background = event.target.value
+
+  }) 
 })
+const activeInput = document.querySelector('input[type="radio"]:checked')
+
+if(activeInput){
+    document.body.style.background = activeInput.value
+}
+
+
+
+
+
+
+
+
+
+
---
+++ js/task-2.js
@@ -0,0 +1,16 @@
+// Напиши скрипт який, при наборі тексту в інпут input#name-input (подія input), підставляє його поточне значення в span#name-output. якщо інпут порожній, в спані повинен відображатися рядок 'незнайомець'.
+
+
+const inputRef = document.querySelector('#name-input')
+const spanRef = document.querySelector("#name-output")
+
+
+inputRef.addEventListener("input", (event)=>{
+    if(event.target.value.trim() === ''){
+        spanRef.textContent = "незнайомець"
+    }else{
+        spanRef.textContent = event.target.value;
+    }
+        // console.log(event.target.value);
+        
+})
