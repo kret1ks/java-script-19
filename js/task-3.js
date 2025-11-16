10150954286482df526cd8ebfce1b8d218922daa
Nazar Bekerskij <bekerskijnazar9@gmail.com>
2025-11-16 21:50:32 +0200

    123

--- js/task-3.js
+++ js/task-3.js
@@ -1,1 +1,18 @@
 // Напиши скрипт, який би при втраті фокуса на інпут, перевіряв його вміст на правильну кількість символів.
+
+
+
+const inputRef = document.querySelector("#validation-input")
+
+
+inputRef.addEventListener("blur", onInputBluer)
+
+function onInputBluer(event){
+    const requiredLenght =Number(event.target.dataset.length)
+    const curentLength = event.target.value.length
+    if(requiredLenght === curentLength){
+        inputRef.classList.add("valid")
+    }else{
+        inputRef.classList.add("invalid")
+    }
+}
