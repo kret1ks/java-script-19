8e40bda2e918f2314c04a808d36fc5d8accfb73b
Nazar Bekerskij <bekerskijnazar9@gmail.com>
2025-11-16 10:45:06 +0200

    123

---
+++ js/task-1.js
@@ -0,0 +1,7 @@
+// 1.Напиши скрипт в якому є три радіобатони зі значенями кольрів. При обиранніна будь якого колір фону боді змінюється відповідно
+
+const inputRef = document.querySelectorAll(".input")
+
+inputRef.addEventListener("click", (event) => {
+        
+})
